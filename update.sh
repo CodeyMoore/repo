3ad147f98485de34a4940f59bd593b1e98285990
Codey Moore <codeymooredev@gmail.com>
2019-06-02 12:07:57 +0930

    .

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ echo
 echo "Rebuilding package list..."
 echo "--------------------------"
 
-#./dpkg-scanpackages.pl -m ./debs /dev/null > Packages
+#./dpkg-scanpackages -m ./debs /dev/null > Packages
 dpkg-scanpackages -m ./debs /dev/null > Packages
 rm -f Packages.bz2
 bzip2 -k Packages
