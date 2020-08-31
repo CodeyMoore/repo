f8a069c64e13c5ae017055e33bba2e8f349bbe01
Codey Moore <codeymooredev@gmail.com>
2020-08-31 18:36:33 +0930

    rip

--- update.sh
+++ update.sh
@@ -8,8 +8,8 @@ echo
 echo "Rebuilding package list..."
 echo "--------------------------"
 
-#./dpkg-scanpackages -m ./debians /dev/null > Packages
-dpkg-scanpackages -m ./debians /dev/null > Packages
+#./dpkg-scanpackages -m ./debs /dev/null > Packages
+dpkg-scanpackages -m ./debs /dev/null > Packages
 rm -f Packages.bz2
 bzip2 -k Packages
 
