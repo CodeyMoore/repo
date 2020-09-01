8534ff48171f8a6c8d26670a72b905cca7c315ce
Codey Moore <codeymooredev@gmail.com>
2020-09-01 18:46:24 +0930

    Back to normal

---
+++ README.md
@@ -0,0 +1,1 @@
+# Codey Moore's APT Repo 
---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo
+
+./clean.sh
+
+echo
+echo "Rebuilding package list..."
+echo "--------------------------"
+
+#./dpkg-scanpackages -m ./debs /dev/null > Packages
+dpkg-scanpackages -m ./debs /dev/null > Packages
+rm -f Packages.bz2
+bzip2 -k Packages
+
+echo "--------------------------"
+echo "Done."
+echo
